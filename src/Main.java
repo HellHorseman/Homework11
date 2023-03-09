146c45f7dbd227744da47502e7e9ff8cdea7622e
HellHorseman <duke.sheffer@yandex.ru>
2023-03-10 03:42:45 +0400

    add task 3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
     public static void initLeapYear() {
         int year = 2021;
@@ -24,9 +25,17 @@ public class Main {
         }
     }
 
+    public static int deliveryDistance() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Укажите дистанцию");
+        int distance = scanner.nextInt();
+        return distance;
+    }
+
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
     public static void task1() {
         System.out.println("Задача 1");
@@ -36,5 +45,22 @@ public class Main {
     public static void task2() {
         System.out.println("Задача 2");
         checkDevice();
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int deliveryDistance = deliveryDistance();
+        int days = 1;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: " + days);
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            days = days + 1;
+            System.out.println("Потребуется дней: " + days);
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            days = days + 2;
+            System.out.println("Потребуется дней: " + days);
+        } else {
+            System.out.println("Доставки нет");
+        }
     }
 }
